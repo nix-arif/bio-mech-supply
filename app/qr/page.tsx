31bd7e119dff9e36039f1988a7a79e52d97f661f
nix_arif <nix_arif@nixs-MacBook-Pro.local>
2026-05-08 14:55:55 +0800

    init

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -4,11 +4,13 @@ export default function QRPage() {
   return (
     <div className="flex items-center justify-between h-screen mx-auto p-10">
       <div>
+        <h1>Bio Mech Supply Sdn Bhd</h1>
         <h1>Best Medical Malaysia</h1>
         <p>
           No. 5-1, Jalan Ungu B U9/B, Seksyen U9, Sunway Kayangan, 40150 Shah
           Alam, Selangor, Malaysia
         </p>
+        <p>MDA Registration No: MDA-10102-K126</p>
       </div>
     </div>
   );
