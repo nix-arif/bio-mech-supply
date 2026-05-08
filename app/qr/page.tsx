7e7e796659d9738ac8297151f42d58877a132336
nix_arif <nix_arif@nixs-MacBook-Pro.local>
2026-05-08 14:45:36 +0800

    init

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -2,8 +2,14 @@ import React from "react";
 
 export default function QRPage() {
   return (
-    <div>
-      <h1>Best Medical Malaysia</h1>
+    <div className="flex items-center justify-between h-screen mx-auto">
+      <div>
+        <h1>Best Medical Malaysia</h1>
+        <p>
+          No. 5-1, Jalan Ungu B U9/B, Seksyen U9, Sunway Kayangan, 40150 Shah
+          Alam, Selangor, Malaysia
+        </p>
+      </div>
     </div>
   );
 }
