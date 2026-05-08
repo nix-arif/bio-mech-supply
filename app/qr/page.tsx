7f08da28d29b2e0f69f0ec2cf57e44365d54e5a2
nix_arif <nix_arif@nixs-MacBook-Pro.local>
2026-05-08 15:12:20 +0800

    init

--- app/qr/page.tsx
+++ app/qr/page.tsx
@@ -1,9 +1,23 @@
-import React from "react";
+import Image from "next/image";
 
 export default function QRPage() {
   return (
     <div className="flex items-center justify-between h-screen mx-auto p-10">
       <div>
+        <div className="flex items-center justify-around">
+          <Image
+            src="/logo.png"
+            width={180}
+            height={180}
+            alt="Best Medical Malaysia Logo"
+          />
+          <Image
+            src="/bestLogo.png"
+            width={350}
+            height={350}
+            alt="Best Medical Malaysia Logo"
+          />
+        </div>
         <h1>Bio Mech Supply Sdn Bhd</h1>
         <h1>Best Medical Malaysia</h1>
         <p>
